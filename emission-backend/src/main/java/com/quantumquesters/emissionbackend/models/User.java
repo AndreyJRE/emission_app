19873e1a8126c50b1fd6edf48cbe3609431a79c5
Kai <mechlerkai@gmail.com>
2024-04-03 16:02:59 +0200

    Created database classes User and Activity

---
+++ emission-backend/src/main/java/com/quantumquesters/emissionbackend/models/User.java
@@ -0,0 +1,39 @@
+package com.quantumquesters.emissionbackend.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user")
+public class User {
+  @Id
+  @GeneratedValue(strategy= GenerationType.IDENTITY)
+  private Long userId;
+
+  @Column(name = "username", length = 50, nullable = false, unique = true)
+  private String userName;
+
+  @OneToMany(mappedBy = "user")
+  private Set<Activity> activities;
+
+  @ManyToMany
+  @JoinTable(
+      name = "friendship",
+      joinColumns = @JoinColumn(name = "user_id"),
+      inverseJoinColumns = @JoinColumn(name = "friend_id")
+  )
+  private Set<User> friends;
+}
