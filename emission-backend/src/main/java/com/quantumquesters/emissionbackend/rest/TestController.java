d5155c8cbace064382e978b1cfcd3c22306378a9
Andrey Stoyanov <astoyano@mail.uni-mannheim.de>
2024-04-03 15:29:09 +0200

    Request Mapping

--- emission-backend/src/main/java/com/quantumquesters/emissionbackend/rest/TestController.java
+++ emission-backend/src/main/java/com/quantumquesters/emissionbackend/rest/TestController.java
@@ -3,9 +3,11 @@ package com.quantumquesters.emissionbackend.rest;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/v1/api/test")
+@RestController
+@RequestMapping("/v1/api/test")
 public class TestController {
 
 
