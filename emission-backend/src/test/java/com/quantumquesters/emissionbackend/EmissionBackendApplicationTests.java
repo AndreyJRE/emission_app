67fda90b959c9ebe4cd2fc7ef9956a53e8798a01
Andrey Stoyanov <astoyano@mail.uni-mannheim.de>
2024-04-03 12:51:13 +0200

    init

---
+++ emission-backend/src/test/java/com/quantumquesters/emissionbackend/EmissionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantumquesters.emissionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmissionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
