ba4839fbf6b8770fe0f5bd21efe2bfc9e488ac53
Andrey Stoyanov <astoyano@mail.uni-mannheim.de>
2024-04-04 11:54:09 +0200

    updated

--- emission-frontend/qq_ui/lib/src/router/User.dart
+++ emission-frontend/qq_ui/lib/src/router/User.dart
@@ -7,7 +7,7 @@ class User{
   User({required this.name, required this.totalEmissions});
 
   factory User.Dummy(String name){
-    return User(name: name, totalEmissions: Random().nextInt(2000).toDouble());
+    return User(name: name, totalEmissions: 0);
   }
 
   factory User.fromJson(Map<String, dynamic> json) {
